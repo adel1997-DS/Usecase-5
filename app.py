941dd2f98eb0648b6ed7c1221bede409487e27ca
adel1997-DS <adel.nawarr1997@gmail.com>
2024-10-27 04:54:41 +0300

    Adel

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.markdown(
+    """
+    <style>
+    body {
+        font-family: 'Arial'; 
+        font-size: 20px; 
+        color: black;
+        direction: rtl;
+        text-align: right; 
+        background-color: #f0f2f6;
+    }
+    p, h2, h3 {
+        font-size: 20px;
+        color: #333;
+    }
+    h1 {
+        text-align: center;
+        color: black;  /* لون العنوان أسود */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.title("تبغى وظيفة؟")
+st.markdown("<hr>", unsafe_allow_html=True)
+
+# نص المحتوى
+st.markdown("**أول شيء:** إذا بتدور وظيفة، ضبط السيفي. **ثاني شيء:** روح للأكثر منطقة فيها توظيف. هنا بنعرض لك أعلى عشر مناطق في التوظيف.")
+st.image("images/نسبة التوظيف.png")
+
+st.markdown("**اخترت المنطقة؟** خلني أطمنك، مهما كان جنسك ترى الطلب عالي على الجنسين. هنا نعرض لك نسبة الطلب.")
+st.image("images/الراتب.png")
+
+st.markdown("**هنا نطاق الرواتب للوظائف.**")
+st.image("images/الخبرة.png")
+
+st.markdown("**السوق يطلب الخريجين الجدد أكثر من الخبرات.**")
+st.image("images/التوظيف حسب الجنس.png")
+
+st.markdown("**بالتوفيق لك في رحلة البحث، وإن شاء الله عطيناك صورة واضحة عن سوق العمل.**")
